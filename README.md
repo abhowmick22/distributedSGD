09b7652886fb7ea8759bb539c2c8f072e3380b12
Abhishek Bhowmick <abhowmick22@gmail.com>
2015-04-14 23:30:35 -0400

    Read input V as tuples

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # distributedSGD
-This is a Spark implementation (Python API) for Distributed Stochastic Gradient Descent based on : "Large-Scale Matrix Factorization with Distributed Stochastic Gradient Descent", Gemulla et al, KDD 2011
+This is a Spark implementation (Python API) for Matrix Factorization using Distributed Stochastic Gradient Descent based on : "Large-Scale Matrix Factorization with Distributed Stochastic Gradient Descent", Gemulla et al, KDD 2011
--- dsgd_mf.py
+++ dsgd_mf.py
@@ -1,6 +1,18 @@
 __author__ = 'abhishek'
 
 import argparse
+from pyspark import SparkContext, SparkConf
+
+
+def parseFile(inputString):
+    lines = inputString.split()
+    movieId = lines[0]
+    reviews = lines[1:]
+    tuples = []
+    for rev in reviews :
+        fields = rev.split(',')
+        tuples.append((fields[0], movieId, fields[1]))    # (movieId, userId, rating) triple
+    return tuples
 
 
 if __name__ == "__main__":
@@ -11,7 +23,7 @@ if __name__ == "__main__":
     '''
 
     # BASE for data -- disable when submitting to Autolab
-    BASE = 'data/'
+    DATA_BASE = 'data/'
 
     # read in params
     parser = argparse.ArgumentParser()
@@ -20,7 +32,7 @@ if __name__ == "__main__":
     parser.add_argument('num_iterations', help='number of iterations for experimental evaluation', type=int)
     parser.add_argument('beta_value', help='beta parameter', type=float)
     parser.add_argument('lambda_value', help='regularization parameter', type=float)
-    parser.add_argument('inputV_filepath', help='file containing training matrix - ratings data', type=str)
+    parser.add_argument('test_filepath', help='file containing test data - ratings data', type=str)
     parser.add_argument('outputW_filepath', help='output file for parameter W', type=str)
     parser.add_argument('outputH_filepath', help='output file for parameter H transpose', type=str)
     args = parser.parse_args()
@@ -30,9 +42,16 @@ if __name__ == "__main__":
     iterations = args.num_iterations
     beta_param = args.beta_value
     lambda_param = args.lambda_value
-    inputV = args.inputV_filepath
+    testFile = args.test_filepath
     outputW = args.outputW_filepath
     outputH = args.outputH_filepath
 
-    
+    # Read the ratings matrix
+    inputFile = DATA_BASE + 'training_set'
+    sc = SparkContext("local", "dsgd")
+    # get a list of tuples (userId, movieId, rating)
+    vTuples = sc.wholeTextFiles(inputFile).flatMap(lambda entry: parseFile(entry[1])).collect()
+
+    print(vTuples)
+
 
