79f6038bf6e76edeb4eff524666033b935ceb5a6
Abhishek Bhowmick <abhowmick22@gmail.com>
2015-04-17 15:14:14 -0400

    Reformat code and add documentation

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # distributedSGD
-This is a Spark implementation (Python API) for Matrix Factorization using Distributed Stochastic Gradient Descent based on : "Large-Scale Matrix Factorization with Distributed Stochastic Gradient Descent", Gemulla et al, KDD 2011
+This is a Spark implementation (Python API) for Matrix Factorization using 
+Distributed Stochastic Gradient Descent based on : "Large-Scale Matrix 
+Factorization with Distributed Stochastic Gradient Descent", Gemulla et al, 
+KDD 2011.
+
+## Dependencies
+Python 2.7, Numpy, Spark 1.3.0
+
+## Instructions
+1. Using submit script (Ensure your PATH points to the spark bin directory)
+spark-submit dsgd_mf.py <num_factors> <num_workers> <num_iterations> \
+<beta_value> <lambda_value> \
+<inputV_filepath> <outputW_filepath> <outputH_filepath>
+
+## Input
+The contents of the input file should be in the following triples format:
+<user_1>,<movie_1>,<rating_11>
+...
+<user_i>,<movie_j>,<rating_ij>
+...
+<user_M>,<movie_N>,<rating_ij>
+
+Where, user i is an integer ID for the ith user, movie j is an integer ID for 
+the jth movie, and rating ij is the rating given by user i to movie j.
+
+For example, 
+1,3114,4
+1,608,4
+1,1246,4
+2,1357,5
+2,3068,4
+2,1537,4
+...
+6040,562,5
+6040,1096,4
+6040,1097,4
+
+## Output
+Factor matrices are written to the specified output files in non-sparse, csv
+format. An example 4X5 matrix:
+3.0,6,9,12,15
+5,10,15.2,20,25
+7,14,21.5,28,35
+11,22.2,33,44.8,55
--- src/dsgd_mf.py
+++ src/dsgd_mf.py
@@ -1,48 +1,50 @@
 #!/usr/bin/env python
 __author__ = 'abhishek'
 
-
 import argparse
 import os
 from pyspark import SparkContext, SparkConf
 import numpy as np
 
-
+# function to extract rating tuples from the training data file
 def parseLine(inputString):
     lines = inputString.split(',')
     return (int(lines[0]), int(lines[1]), float(lines[2]))
 
-def sgd(V, W, H, workerId):
+# function to perform SGD updates
+def sgd(V, W, H):
     Wmapped = {int(key):value for (key, value) in [(elem[0], elem[1]) for elem in W]}
     Hmapped = {int(key):value for (key, value) in [(elem[0], elem[1]) for elem in H]}
-    nprime = 0
-    n = global_updates_b.value
+    nprime = 0                                                                           # Num SGD updates
+    n = global_updates_b.value                                                           # updates performed before this iteration
     for rating in V:
         vij = rating[2]
-        Wi = Wmapped[rating[0]]
-        Hj = Hmapped[rating[1]]
-        whProd = np.dot(Wi, Hj)
+        w_i = Wmapped[rating[0]]
+        h_j = Hmapped[rating[1]]
+        whProd = np.dot(w_i, h_j)
         factor = (whProd - vij)*2
-        Wloss = factor*Hj + (2*LAMBDA/Ni[rating[0]]) * Wi
-        Hloss = factor*Wi + (2*LAMBDA/Nj[rating[1]]) * Hj
+        w_loss = factor*h_j + (2*LAMBDA/Ni[rating[0]]) * w_i
+        h_loss = factor*w_i + (2*LAMBDA/Nj[rating[1]]) * h_j
         epsilon = pow((n + nprime + tau.value), -beta.value)
-        Wprime = Wi - epsilon*Wloss
-        Hprime = Hj - epsilon*Hloss
-        Wmapped[rating[0]] = Wprime
-        Hmapped[rating[1]] = Hprime
+        w_prime = w_i - epsilon*w_loss
+        h_prime = h_j - epsilon*h_loss
+        Wmapped[rating[0]] = w_prime
+        Hmapped[rating[1]] = h_prime
         nprime += 1
-    # update epsilon globally
+    # add num of SGD updates to global counter
     global_updates_a.add(nprime)
     return Wmapped, Hmapped
 
+# map function to be applied to each partition
 def myMapFunc(iterator):
     result = []
     for elem in iterator:
         workerId = elem[0]
-        V = sorted(list([item for item in elem[1][0] if (item[1]-1)%NUM_WORKERS == (workerId + stratum)%NUM_WORKERS]))  # Extract and filter V
-        W = sorted(list(elem[1][1]))                                                                         # Extract W
-        H = sorted(list(elem[1][2]))                                                                         # Extract H
-        Wnew, Hnew = sgd(V, W, H, workerId)
+        V = list([item for item in elem[1][0] if
+                         (item[1]-1)%NUM_WORKERS == (workerId + stratum)%NUM_WORKERS])               # Extract and filter V
+        W = list(elem[1][1])                                                                         # Extract W
+        H = list(elem[1][2])                                                                         # Extract H
+        Wnew, Hnew = sgd(V, W, H)
         yield (Wnew, Hnew)
 
 
@@ -52,10 +54,6 @@ if __name__ == "__main__":
     <beta_value> <lambda_value> \
     <inputV_filepath> <outputW_filepath> <outputH_filepath>
     '''
-
-    # BASE for data -- disable when submitting to Autolab
-    DATA_BASE = 'data/'
-
     # read in params
     parser = argparse.ArgumentParser()
     parser.add_argument('num_factors', help='the rank for decomposition', type=int)
@@ -78,57 +76,58 @@ if __name__ == "__main__":
     HFILE = args.outputH_filepath
     TAU = 100
 
-    # Read the ratings matrix
+    # Read the ratings matrix into an RDD
     sc = SparkContext("local", "dsgd")
     vTuples = sc.textFile(INPUTFILE).map(lambda line: parseLine(line))
 
+    # Determine dimensions of ratings matrix
     NUM_COLS = vTuples.map(lambda x : x[1]).max()
     NUM_ROWS = vTuples.map(lambda x : x[0]).max()
+    NUM_STRATA = NUM_WORKERS                                            # Number of strata
 
-    # Create and initialize W and H matrices
+    # Create and initialize factor matrices
     Wcurr = np.random.ranf(size=(NUM_ROWS, FACTORS))                    # Num of Users X Factors = W
     Hcurr = np.random.ranf(size=(NUM_COLS, FACTORS))                    # Num of Users X Factors = H'
-
-    NUM_STRATA = NUM_WORKERS                                             # Number of strata
 
     # Build keys for W
     rowIndices = range(1, NUM_ROWS+1)
     colIndices = range(1, NUM_COLS+1)
 
-    # Compute and broadcast Ni and Nj
+    # Compute and broadcast Ni*/N*j, ie num of non zero elements per row/column in rating matrix
     Ni = vTuples.keyBy(lambda x : x[0]).map(lambda x : (x[0], 1)).reduceByKey(lambda a, b : a + b).collectAsMap()
     Nj = vTuples.keyBy(lambda x : x[1]).map(lambda x : (x[0], 1)).reduceByKey(lambda a, b : a + b).collectAsMap()
 
     # Broadcast TAU and BETA, we didn't know why we do this, perhaps it works by just being in scope
     tau = sc.broadcast(TAU)
     beta = sc.broadcast(BETA)
-    #f = open('output', 'w')
-    Wpaired = zip(rowIndices, Wcurr)                 # list
+
+    # Pair factor matrices with their indices, this is useful later for recurring updates
+    Wpaired = zip(rowIndices, Wcurr)
     Hpaired = zip(colIndices, Hcurr)
 
+    # accumulator variables to keep track of SGD updates across iterations
     accumulated = 0
     global_updates_a = sc.accumulator(0)
 
     for epoch in range(1, ITERATIONS+1):
         global_updates_b = sc.broadcast(accumulated)
         for stratum in range(NUM_STRATA):
-            # Build keyed version of W
-            W_keyed = sc.parallelize(Wpaired).keyBy(lambda x : (x[0]-1) % NUM_WORKERS).partitionBy(NUM_WORKERS)     # perhaps optimize here more
+            # Build keyed version of W, perhaps we can optimize by persisting these partitioned RDDs on local workers
+            W_keyed = sc.parallelize(Wpaired).keyBy(lambda x : (x[0]-1) % NUM_WORKERS).partitionBy(NUM_WORKERS)
             # Build keyed version of H
             H_keyed = sc.parallelize(Hpaired).keyBy(lambda x : (NUM_WORKERS+((x[0]-1)%NUM_WORKERS)-stratum) % NUM_WORKERS).partitionBy(NUM_WORKERS)
-            # Build keyed version of V (only based on row, columns will be filtered out in the map function)
-            V_keyed = vTuples.keyBy(lambda x : (x[0]-1) % NUM_WORKERS).partitionBy(NUM_WORKERS)                  # perhaps optimize here more
+            # Build keyed version of V, perhaps we can optimize by persisting these partitioned RDDs on local workers
+            V_keyed = vTuples.keyBy(lambda x : (x[0]-1) % NUM_WORKERS).partitionBy(NUM_WORKERS)
             # Group W, H and V
             result = list(V_keyed.groupWith(W_keyed, H_keyed).mapPartitions(myMapFunc).collect())
-            # list of NUM_WORKERS lists, each containing of 1 tuple of 2 elements -> (index, row/column as list of values)
-            # accumulate all the results into Wpaired and Hpaired
+            # accumulate all the results from worker nodes
             Wmapped = {}
             Hmapped = {}
             for res in result:
                 Wmapped.update(res[0])
                 Hmapped.update(res[1])
-            Wpaired = Wmapped.items()      #  list of tuples
-            Hpaired = Hmapped.items()      #  list of tuples
+            Wpaired = Wmapped.items()
+            Hpaired = Hmapped.items()
         accumulated = global_updates_a.value
         global_updates_b.unpersist()
 
@@ -138,25 +137,18 @@ if __name__ == "__main__":
     for row in W:
         np.savetxt(f, row, fmt="%.1f", delimiter=',')
     f.close()
+    # remove last newline character in file
     with open(WFILE, 'rb+') as filehandle:
         filehandle.seek(-1, os.SEEK_END)
         filehandle.truncate()
 
-    # Write out H after transpose
+    # Write out H' after transposing
     H_transpose = np.matrix([col[1] for col in sorted(Hpaired, key = lambda tup : tup[0])]).transpose()
     f = open(HFILE, 'w')
     for col in H_transpose:
         np.savetxt(f, col, fmt="%.1f", delimiter=',')
     f.close()
+    # remove last newline character in file
     with open(HFILE, 'rb+') as filehandle:
         filehandle.seek(-1, os.SEEK_END)
         filehandle.truncate()
-
-
-
-
-
-
-
-
-
